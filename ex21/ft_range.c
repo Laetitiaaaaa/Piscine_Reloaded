5098ba5af7a17d10145d14f35e81f77adf042b24
Laetitia LEJEUNE <llejeune@e3r8p1.42.fr>
2019-04-25 11:08:22 +0200

    last commit

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: llejeune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 12:35:46 by llejeune          #+#    #+#             */
-/*   Updated: 2018/11/09 14:54:39 by llejeune         ###   ########.fr       */
+/*   Updated: 2018/12/04 08:29:07 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ int		*ft_range(int min, int max)
 		tab = (int*)malloc(sizeof(int) * taille);
 	if (tab == 0)
 		return (0);
-	tab[0] = min;
-	i = 1;
-	while ((i + min) < max)
+	i = 0;
+	while (min < max)
 	{
-		tab[i] = i + min;
+		tab[i] = min;
 		i++;
+		min++;
 	}
 	return (tab);
 }
